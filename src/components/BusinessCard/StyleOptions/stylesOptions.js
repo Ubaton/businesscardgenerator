f564b4400e21861a127e71e4a6c62a792a1d3acc
Raymond <32289508+Ubaton@users.noreply.github.com>
2024-06-29 18:46:06 +0200

    Pattens styles add for the business cards

--- src/components/BusinessCard/StyleOptions/stylesOptions.js
+++ src/components/BusinessCard/StyleOptions/stylesOptions.js
@@ -1,35 +1,43 @@
 const styleOptions = [
   {
     name: "Violet 1",
-    className: "bg-violet-500 p-4 rounded-lg shadow-md",
+    className:
+      "bg-violet-500 p-4 rounded-lg shadow-md card-pattern-violet business-card",
   },
   {
     name: "Blue 2",
-    className: "bg-blue-500 p-4 rounded-lg shadow-md",
+    className:
+      "bg-blue-500 p-4 rounded-lg shadow-md card-pattern-blue business-card",
   },
   {
     name: "Gray 3",
-    className: "bg-gray-500 p-4 rounded-lg shadow-md",
+    className:
+      "bg-gray-500 p-4 rounded-lg shadow-md card-pattern-gray business-card",
   },
   {
     name: "Rose 4",
-    className: "bg-rose-500 p-4 rounded-lg shadow-md",
+    className:
+      "bg-rose-500 p-4 rounded-lg shadow-md card-pattern-rose business-card",
   },
   {
     name: "Green 5",
-    className: "bg-green-500 p-4 rounded-lg shadow-md",
+    className:
+      "bg-green-500 p-4 rounded-lg shadow-md card-pattern-green business-card",
   },
   {
     name: "Red 6",
-    className: "bg-red-500 p-4 rounded-lg shadow-md",
+    className:
+      "bg-red-500 p-4 rounded-lg shadow-md card-pattern-red business-card",
   },
   {
     name: "Amber 7",
-    className: "bg-amber-500 p-4 rounded-lg shadow-md",
+    className:
+      "bg-amber-500 p-4 rounded-lg shadow-md card-pattern-amber business-card",
   },
   {
     name: "Black 8",
-    className: "bg-zinc-900 p-4 rounded-lg shadow-md",
+    className:
+      "bg-zinc-900 p-4 rounded-lg shadow-md card-pattern-black business-card",
   },
 ];
 
